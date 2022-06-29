e3b40d8d59062d90afaf58ac4f9edb50538cec85
Gurkirat Singh <gurkiratsingh@Gurkirats-MacBook-Air.local>
2022-06-30 00:25:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+
+const app=express()
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set('view engine','ejs');
+
+
+app.get("/",function(req,res){
+  res.render("home");
+})
+app.get("/signin",function(req,res){
+  res.render("signin");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+  console.log("starting the server");
+})
