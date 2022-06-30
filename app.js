efe2ac89341891ae8115511d89e1a0392601b30d
Gurkirat Singh <gurkiratsingh@Gurkirats-MacBook-Air.local>
2022-06-30 23:26:11 +0530

    automatic email at register

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
 
+let email="";   // initial the email.......
+
+
+
 
 app.get("/",function(req,res){
   res.render("home");
@@ -14,8 +18,14 @@ app.get("/",function(req,res){
 app.get("/signin",function(req,res){
   res.render("signin");
 })
-
-
+app.get("/register",(req,res)=>{
+  res.render("register",{email:email});
+})
+app.post("/",function(req,res){
+  console.log(req.body);
+  email=req.body.email;     //getting the value of email from home page and sending it to register for passsword
+  res.redirect("register");
+})
 
 
 
@@ -29,4 +39,3 @@ app.get("/signin",function(req,res){
 app.listen(3000,function(){
   console.log("starting the server");
 })
- 
