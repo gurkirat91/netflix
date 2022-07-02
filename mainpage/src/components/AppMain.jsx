8a08c3b9894fed822df63659e641b5e13c23479c
Rupinder <rupinderkaur8804@gmail.com>
2022-07-02 18:54:20 +0530

    React folder

---
+++ mainpage/src/components/AppMain.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Content  from "./Content";
+
+function AppMain(){
+    return(
+        <div>
+        <Navbar />
+        <Content />
+        </div>
+    );
+}
+export default AppMain;
